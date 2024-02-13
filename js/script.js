ce12997c87037c2f8bf4ba8ade15da1136e186f9
mario.arista <mario.arista.dev@gmail.com>
2024-02-13 14:59:08 +0100

    MIlestone 0

---
+++ js/script.js
@@ -0,0 +1,54 @@
+/*
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede
+
+*/
+
+/*
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+*/
+const businessCards = [
+    {
+        fullname: "Wayne Barnett",
+        position:"Founder & CEO",
+        image: `wayne-barnett-founder-ceo.jpg`
+    },
+    {
+        fullname: "Angela Caroll",
+        position:"Chief Editor",
+        image: `angela-caroll-chief-editor.jpg`
+    },
+    {
+        fullname: "Walter Gordon",
+        position:"Office Manager",
+        image: `walter-gordon-office-manager.jpg`
+    },
+    {
+        fullname: "Angela Lopez",
+        position:"Social Media Manager",
+        image: `angela-lopez-social-media-manager.jpg`
+    },
+    {
+        fullname: "Scott Estrada",
+        position:"Developer",
+        image: `scott-estrada-developer.jpg`
+    },
+    {
+        fullname: "Barbara Ramos",
+        position:"Graphic Designer",
+        image: `barbara-ramos-graphic-designer.jpg`
+    },
+
+];
+
+console.log(businessCards);
+
+
+	 	     
